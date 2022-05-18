1b742a8fa010ccbf54d9027715c840d077da6fd1
Natali <m.n.a1881@mail.ru>
2022-05-18 15:06:55 +0400

    click

--- site/btn.js
+++ site/btn.js
@@ -1,3 +1,10 @@
 const btn1 = document.querySelector('#btn1');
 const btn2 = document.querySelector('#btn2');
 
+btn1.addEventListener("click", function(){
+    console.log('clicked');
+});
+
+btn2.addEventListener("click", function(){
+    console.log('clicked');
+});
